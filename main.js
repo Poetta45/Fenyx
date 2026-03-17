c712b12f986aa2c35ad35429394eb348b348d5b6
Poetta45 <guilhermespm8888@gmail.com>
2026-03-17 10:21:23 -0300

    V1.0

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        System.console.log("Hello, World!");
-    }}
+class Main{
+    static main(){
+        console.log("Hello, World!");
+    }
+}
